8ce9ac7283263176a96bc84f8f60e2254430ec21
Prenom Nom <email@domaine.com>
2024-10-30 09:54:07 +0100

    port 5050

--- sample-app.sh
+++ sample-app.sh
@@ -13,12 +13,12 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "EXPOSE 6060" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker run -t -d -p 6060:6060 --name samplerunning sampleapp
 docker ps -a 
 
 
